b566733cb3f5a6b7ec3ef2b9c52589df6f1a79cb
antran <andevstudioth@gmail.com>
2026-06-03 13:53:58 +0800

    Rename project to VersionDashboard

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "KotlinProject"
+rootProject.name = "VersionDashboard"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
--- shared/src/commonMain/kotlin/org/example/project/App.kt
+++ shared/src/commonMain/kotlin/org/example/project/App.kt
@@ -16,8 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
 
-import kotlinproject.shared.generated.resources.Res
-import kotlinproject.shared.generated.resources.compose_multiplatform
+import versiondashboard.shared.generated.resources.Res
+import versiondashboard.shared.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
